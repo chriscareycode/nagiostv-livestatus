6783419d442c368662dce342e886d839c86cc36f
Chris Carey <chris.carey@gmail.com>
2014-02-17 20:52:40 -0800

    fix nagios nagios3 path issue in live.php

--- php-api/live.php
+++ php-api/live.php
@@ -2,8 +2,14 @@
 
 $command = $_GET['command'];       
 $command = urldecode($command);        
-//$command = escapeshellcmd($command);        
-$cmd = "echo '".$command."' | unixcat /var/lib/nagios/rw/live";
+//$command = escapeshellcmd($command);   
+if (file_exists('/var/lib/nagios/rw/live')) {
+	$cmd = "echo '".$command."' | unixcat /var/lib/nagios/rw/live";
+}
+if (file_exists('/var/lib/nagios3/rw/live')) {
+	$cmd = "echo '".$command."' | unixcat /var/lib/nagios3/rw/live";
+}     
+
 passthru($cmd);
 
 ?>
