6186323391035efe0d6021060383ae69152bac1d
Chris Carey <chris.carey@gmail.com>
2014-02-22 19:07:17 -0800

    let us not forget these updates to livestatus.php api

--- php-api/livestatus.php
+++ php-api/livestatus.php
@@ -12,7 +12,10 @@ if (!$q) {
 
 
 }
-//if ($q=="services") 
+//if ($q=="/services") $q = ltrim($q, '/');
+//if ($q=="/servicenocaches") $q = ltrim($q, '/');
+$q = ltrim($q, '/');
+
 $command = "GET services\nFilter: state = 1\nFilter: state = 2\nFilter: state = 3\nOr: 3\nOutputFormat: json";
 
 //$command = $_GET['command'];       
@@ -26,13 +29,55 @@ if (file_exists('/var/lib/nagios/rw/live')) {
 if (file_exists('/var/lib/nagios3/rw/live')) {
   $cmd = "echo '".$command."' | unixcat /var/lib/nagios3/rw/live";
 }     
-passthru($cmd);
+//passthru($cmd);
 
 
+//loop through each item and output it in a format that makes EmberJS ember-data happy
+$data = shell_exec($cmd);
+//exec($cmd, $data);
+//print('raw data time');
+//print_r($data);
 
+//$data_combined = $data[0].$data[1];
+//$data_obj = json_decode($data_combined);
+$data_obj = json_decode($data);
+//$data_obj = $data;
+/*
+print('sizes');
+//print_r($data_obj);
+print('data_obj:'.sizeof($data_obj));
+print(' data_obj0:'.sizeof($data_obj[0]));
+print(' data_obj1:'.sizeof($data_obj[1]));
+//print_r($data_obj[1]);
+*/
+$id=1;
+$result = array();
+for ($i=1;$i<sizeof($data_obj);$i++) {
+  $inner = array();
+  for($j=0;$j<sizeof($data_obj[0]);$j++) {
+    //print('testy loop '.$i.' row '.$j);
+    //print_r($data_obj[$i][$j]);
 
+    $inner[$data_obj[0][$j]] = $data_obj[$i][$j]; 
+  }
+  $inner['id'] = $id; 
+  array_push($result, $inner);
+  
+  //$inner['id'] = ++$id; array_push($result, $inner);
+  //$inner['id'] = ++$id; array_push($result, $inner);
+  //$inner['id'] = ++$id; array_push($result, $inner);
 
-
+  $id++;
+}
+$json = json_encode(
+    array(
+        //"services" => array(
+        //    $result
+        //)
+        $q => $result
+    )
+);
+/*
 $json = json_encode(
     array(
         "services" => array(
@@ -42,6 +87,7 @@ $json = json_encode(
         )
     )
 );
+*/
 
 echo $json;
 
